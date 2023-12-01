445fd12b3ebc389948591a8b853ff5b2cf3f3ebe
PumkinB <olipumknb@gmail.com>
2023-12-01 22:11:06 +0700

    third commit

--- kalkulator.js
+++ kalkulator.js
@@ -1,12 +1,9 @@
-function klik() {
-    var a = parseFloat(document.getElementById("input-field").value);
-    var n = parseFloat(document.getElementById("input-field2").value);
+let inputAngka = document.getElementById("input-field");
+let inputPangkat = document.getElementById("input-field2");
 
-    //validasi
-    if (isNaN(a) || (isNaN(n))){
-      document.getElementById("output-field").value = "Yang anda masukkan adalah bukan angka";
-      return;
-    }
+function klik() {
+    var a = parseFloat(inputAngka.value);
+    var n = parseFloat(inputPangkat.value);
 
     // Menghitung turunan fungsi
     var pangkat = n - 1;
@@ -18,27 +15,9 @@ function klik() {
     // Cara menghitung turunan fungsi (Calculation)
     document.getElementById("output-field2").value = "f(x) =" + a + "." + n + "x^" + n + "-1";
 
-  };
-    function pencet(){
-    var n = parseFloat(document.getElementById("input-field2").value);
-
-   //Pangkat doang
-   var pangkatOnly = n - 1;
-   var angka2 = n;
-   var hasil2 = angka2 + "x^" + pangkatOnly;
-   
-   document.getElementById("output-field").value = hasil2;
-
-   // Cara menghitung turunan fungsi (Calculation)
-   document.getElementById("output-field2").value = "f(x) =" + n  + "x^" + n + "- 1";
-      
-  };
-
-  function res(){
-    var a = document.getElementById("input-field").value = "";
-    var n = document.getElementById("input-field2").value = "";
-    var hasil = document.getElementById("output-field").value = "0";
-    document.getElementById("output-field2").value = "-";
+  //Menghapus field
+    inputAngka.value = "";
+    inputPangkat.value = "";
   };
 
 
